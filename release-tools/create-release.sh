bdd512edd73d007c7b589f8918a3f17caec9fee2
Brendan Quinn <brendan@cluefulmedia.com>
2021-10-04 12:16:30 +0300

    Small updates to release process and script

--- release-tools/create-release.sh
+++ release-tools/create-release.sh
@@ -5,42 +5,42 @@ echo "Loading shared environment variables including new NewsML-G2 version numbe
 . release-tools/newsmlg2-config-vars.sh
 
 echo "Emptying folder in case it already existed"
-rm -fr releases/$NEWSMLG2_VERSION
-rm -fr releases/NewsML-G2_$NEWSMLG2_VERSION*.zip
+rm -fr releases/$NEW_NEWSMLG2_VERSION
+rm -fr releases/NewsML-G2_$NEW_NEWSMLG2_VERSION*.zip
 
-echo "Creating release folder"
-mkdir -p releases/$NEWSMLG2_VERSION
+echo "Creating release folder releases/$NEW_NEWSMLG2_VERSION"
+mkdir -p releases/$NEW_NEWSMLG2_VERSION
 
 echo "Copying release files to the release folder"
-cp -r documentation examples LICENSE releases/$NEWSMLG2_VERSION/
+cp -r documentation examples LICENSE releases/$NEW_NEWSMLG2_VERSION/
 
 echo "Copying README files to the release folder"
-python -m markdown README.md >releases/$NEWSMLG2_VERSION/README.html 
-python -m markdown documentation/NewsML-G2-documentation.md >releases/$NEWSMLG2_VERSION/documentation/index.html
+python -m markdown README.md >releases/$NEW_NEWSMLG2_VERSION/README.html
+python -m markdown documentation/NewsML-G2-documentation.md >releases/$NEW_NEWSMLG2_VERSION/documentation/index.html
 
 echo "Copying specification (minus XML Schema docs) to the release folder"
-mkdir -p releases/$NEWSMLG2_VERSION/specification
-cp -r specification/NewsML-G2_2*Power.xsd specification/XML-Schema_FileVersion_* specification/individual specification/xml.xsd releases/$NEWSMLG2_VERSION/specification/
+mkdir -p releases/$NEW_NEWSMLG2_VERSION/specification
+cp -r specification/NewsML-G2_2*Power.xsd specification/XML-Schema_FileVersion_* specification/individual specification/xml.xsd releases/$NEW_NEWSMLG2_VERSION/specification/
 
 ZIP_PATH=`which zip`
 
 cd releases
 echo "Creating ZIP file (version without XML Schema documentation) using $ZIP_PATH"
-$ZIP_PATH -r -9 NewsML-G2_$NEWSMLG2_VERSION-noXMLdocu.zip $NEWSMLG2_VERSION/documentation $NEWSMLG2_VERSION/specification $NEWSMLG2_VERSION/examples $NEWSMLG2_VERSION/LICENSE $NEWSMLG2_VERSION/NewsML-G2-README.md
+$ZIP_PATH -r -9 NewsML-G2_$NEW_NEWSMLG2_VERSION-noXMLdocu.zip $NEW_NEWSMLG2_VERSION/documentation $NEW_NEWSMLG2_VERSION/specification $NEW_NEWSMLG2_VERSION/examples $NEW_NEWSMLG2_VERSION/LICENSE $NEW_NEWSMLG2_VERSION/README.md $NEW_NEWSMLG2_VERSION/README.html
 cd ..
 
 echo "Copying XML Schema documentation to release folder"
-cp -r specification/XML-Schema-Doc-Power releases/$NEWSMLG2_VERSION/specification/
+cp -r specification/XML-Schema-Doc-Power releases/$NEW_NEWSMLG2_VERSION/specification/
 
 echo "Creating ZIP file (version with documentation) using $ZIP_PATH"
 cd releases
-$ZIP_PATH -r -9 NewsML-G2_$NEWSMLG2_VERSION.zip $NEWSMLG2_VERSION/documentation $NEWSMLG2_VERSION/specification $NEWSMLG2_VERSION/examples $NEWSMLG2_VERSION/LICENSE $NEWSMLG2_VERSION/NewsML-G2-README.md
+$ZIP_PATH -r -9 NewsML-G2_$NEW_NEWSMLG2_VERSION.zip $NEW_NEWSMLG2_VERSION/documentation $NEW_NEWSMLG2_VERSION/specification $NEW_NEWSMLG2_VERSION/examples $NEW_NEWSMLG2_VERSION/LICENSE $NEW_NEWSMLG2_VERSION/README.md $NEW_NEWSMLG2_VERSION/README.html
 cd ..
 
 echo "Creating ZIP file of documentation alone using $ZIP_PATH"
-cd releases/$NEWSMLG2_VERSION/specification
+cd releases/$NEW_NEWSMLG2_VERSION/specification
 $ZIP_PATH -r -9 XML-Schema-Doc-Power.zip XML-Schema-Doc-Power
 cd ../../..
 
 echo "Done."
-echo "Now copy releases/NewsML-G2_$NEWSMLG2_VERSION-noXMLdocu.zip, releases/NewsML-G2_$NEWSMLG2_VERSION.zip and the releases/$NEWSMLG2_VERSION folder to iptc.org/std."
+echo "Now copy releases/NewsML-G2_$NEW_NEWSMLG2_VERSION-noXMLdocu.zip, releases/NewsML-G2_$NEW_NEWSMLG2_VERSION.zip and the releases/$NEW_NEWSMLG2_VERSION folder to iptc.org/std"
